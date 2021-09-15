910b5ac8bc5b42a2cf1bc910ea0972823d1657c7
Andrew <andrewmgunn26@gmail.com>
2021-09-15 12:49:13 -0500

    finished intrusion report method for parallel array sorting, max, min, sum

--- Program.cs
+++ Program.cs
@@ -1,13 +1,51 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Channels;
 
 namespace Module5Lecture2
 {
     class Program
     {
+        static void IntrusionReport()
+        {
+            string[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+            int[] intrusions = new int[days.Length];
+
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.Write($"{days[i]} intrusions>");
+                Int32.TryParse(Console.ReadLine(), out intrusions[i]);
+            }
+            
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine($"{days[i]}: {intrusions[i]}");
+            }
+
+            Console.WriteLine($"Total: {intrusions.Sum()}");
+            Console.WriteLine($"Average: {intrusions.Sum() / (double)intrusions.Length}");
+            Console.WriteLine();
+            
+            
+            /*
+             * To get Min/max we sort both arrays first
+             * Then getting first and last items where first is min and last is max
+             *
+             * Array.length - 1 works because the length will be 1 longer than the indexes
+             * which start at 0.
+             */
+            Array.Sort(intrusions, days);
+
+            
+            Console.WriteLine($"{days[days.Length - 1]} had {intrusions[intrusions.Length - 1]}");
+            Console.WriteLine($"{days[0]} had the least: {intrusions[0]}");
+
+        }
+
+
         static void Main(string[] args)
         {
-            
-            
+            IntrusionReport();
         }
     }
 }
